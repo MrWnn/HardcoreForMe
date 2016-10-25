8bb1922fd42bf95112b2bf13bf83eec9912ab51a
Nick Wnn <wnn5191991@163.com>
2016-10-25 14:58:15 +0800

    check_ipv4_format

---
+++ Shell_Scripts/check_if_ip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CHECK_IF_IP()
+{
+        IP=$1
+        if [[ `echo ${IP}|grep "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]
+        then
+                echo -e "\nRequire valid IP input\n"
+                exit
+        fi
+}
+CHECK_IF_IP $1
