96bf0850f1cb5309ad17d73fa0049597921d4f4a
Nick Wnn <wnn5191991@163.com>
2016-12-02 10:26:27 +0800

    Update check_if_ip.sh

--- Shell_Scripts/check_if_ip.sh
+++ Shell_Scripts/check_if_ip.sh
@@ -2,7 +2,7 @@
 CHECK_IF_IP()
 {
         IP=$1
-        if [[ `echo ${IP}|grep "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]
+        if [[ `echo ${IP}|grep "^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]
         then
                 echo -e "\nRequire valid IP input\n"
                 exit
