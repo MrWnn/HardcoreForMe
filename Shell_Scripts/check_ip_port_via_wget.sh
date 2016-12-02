0e2853243314cfd8404965504e868955256a7bca
Nick Wnn <wnn5191991@163.com>
2016-12-02 10:15:01 +0800

    check ip port via wget

---
+++ Shell_Scripts/check_ip_port_via_wget.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CHECK_IP=$1
+CHECK_PORT
+STATUS=1
+
+CHECK_WGET=`whereis wget |grep \/wget|wc -l`
+if [[ ${CHECK_WGET} == 0 ]]
+then 
+  echo "wget not exist, exit..."
+  exit
+elif [[ ${CHECK_WGET} == "" ]]
+then
+  echo "check status empty, plz recheck wget"
+  exit
+else
+  wget --spider -t1 ${CHECK_IP}:${CHECK_PORT} || STATUS=0
+fi
+
+
+if [[ ${STATUS} == "0" ]]
+then 
+  echo "${CHECK_IP} at ${CHECK_PORT} fail"
+elif [[ ${STATUS} == "" ]]
+then
+  echo "STATUS empty"
+else
+  echo "${CHECK_IP} at ${CHECK_PORT} access"
+fi
