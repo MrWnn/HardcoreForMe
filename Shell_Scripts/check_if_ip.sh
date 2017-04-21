de05ee7b0c502257372e79917590b39b481b2b2d
Nick Wnn <wnn5191991@163.com>
2017-04-21 19:19:09 +0800

    fix  bu9
    fix bu9

--- Shell_Scripts/check_if_ip.sh
+++ Shell_Scripts/check_if_ip.sh
@@ -2,7 +2,7 @@
 CHECK_IF_IP()
 {
         IP=$1
-        if [[ `echo ${IP}|grep "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]00
+        if [[ `echo ${IP}|grep "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]
         then
                 echo -e "\nRequire valid IP input\n"
                 exit
@@ -11,7 +11,7 @@ CHECK_IF_IP()
                 B_part=`echo ${IP}|awk -F. '{print $2}'`
                 C_part=`echo ${IP}|awk -F. '{print $3}'`
                 D_part=`echo ${IP}|awk -F. '{print $4}'`
-                for parts in A_part B_part C_part D_part 
+                for parts in ${A_part} ${B_part} ${C_part} ${D_part} 
                 do 
                         if [ ${parts} -ge 255 ]||[ ${parts} -le 0 ]|| [ `echo ${parts} |grep ^0 |wc -l` -eq 1  ] 
                         then 
