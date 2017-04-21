95d52bbccfd98d689276432c2667b46976bb856f
Nick Wnn <wnn5191991@163.com>
2017-04-21 19:07:45 +0800

    Update check_if_ip.sh
    add A B C D part rule check

--- Shell_Scripts/check_if_ip.sh
+++ Shell_Scripts/check_if_ip.sh
@@ -2,10 +2,22 @@
 CHECK_IF_IP()
 {
         IP=$1
-        if [[ `echo ${IP}|grep "^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]
+        if [[ `echo ${IP}|grep "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$"|wc -l` != 1 ]]00
         then
                 echo -e "\nRequire valid IP input\n"
                 exit
+        else 
+                A_part=`echo ${IP}|awk -F. '{print $1}'`
+                B_part=`echo ${IP}|awk -F. '{print $2}'`
+                C_part=`echo ${IP}|awk -F. '{print $3}'`
+                D_part=`echo ${IP}|awk -F. '{print $4}'`
+                for parts in A_part B_part C_part D_part 
+                do 
+                        if [ ${parts} -ge 255 ]||[ ${parts} -le 0 ]|| [ `echo ${parts} |grep ^0 |wc -l` -eq 1  ] 
+                        then 
+                                echo -e "\n Invalid IP ${IP} input"
+                                exit
+                        fi
         fi
 }
 CHECK_IF_IP $1
