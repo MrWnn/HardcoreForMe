b6d2b959c2f22c693a0b9d609d3c4618a3d27eb2
Nick Wnn <wnn5191991@163.com>
2017-07-09 20:56:51 +0800

    add reminder of syntax python

---
+++ python/reminder.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+_author_ = 'mrwnn'
+# a=1
+# print("")
+# print(a.upper())
+# print(" asd".lstrip())
+# print("a"+"b")
+# import this
+# cmd_list = ['ls','pwd','vnstat','iostat']
+
+print("hello asdasd")
+print("hello\nasdasd")
+a="asd"
+print(a.upper())
+print(" asd asd ".lstrip())
+print("a"+"b")
+cmd_list = ['ls','pwd','vnstat','iostat']
+print(cmd_list)
+print(cmd_list[0])
+print(cmd_list[1])
+for i in range(0,len(cmd_list)):
+    print(cmd_list[i])
