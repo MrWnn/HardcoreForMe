d9037543ea9c6ab6cf1b9fa65b4950700d76a1d1
Nick Wnn <wnn5191991@163.com>
2017-07-13 15:33:58 +0800

    update reminder

--- python/reminder.py
+++ python/reminder.py
@@ -8,15 +8,30 @@ _author_ = 'mrwnn'
 # import this
 # cmd_list = ['ls','pwd','vnstat','iostat']
 
+# print strings
 print("hello asdasd")
 print("hello\nasdasd")
+
+# variable operator
 a="asd"
 print(a.upper())
 print(" asd asd ".lstrip())
 print("a"+"b")
+
+#list operator
 cmd_list = ['ls','pwd','vnstat','iostat']
 print(cmd_list)
 print(cmd_list[0])
 print(cmd_list[1])
+
+sort_list = [ '6','3','5','4' ]
+print sort_list
+sort_list.sort()
+print sort_list
+
 for i in range(0,len(cmd_list)):
     print(cmd_list[i])
+
+# triple subjects operator
+b=1;c=2
+print b>c and b or c 
